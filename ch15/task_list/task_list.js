c28c29623e1153a03277903b34a102b6976b57f0
Tziporah <tziporawerzberger@gmail.com>
2021-04-30 12:57:57 -0400

    adds chapter 15 exercises

--- ch15/task_list/task_list.js
+++ ch15/task_list/task_list.js
@@ -1,11 +1,20 @@
 "use strict";
-
 $(document).ready(function(){
-	var displayTasks = function() {
-        $("#task_list").val( localStorage.E15tasks );
+    var displayTasks = function() {
+        //$("#task_list").val( localStorage.tasks );
+        
+        var expiration = new Date( localStorage.expiration );
+        var today = new Date();
+        if ( expiration.getTime() < today.getTime() ) {
+            localStorage.removeItem( "S15tasks" );
+            localStorage.removeItem( "expiration" );
+        } else {
+            $("#task_list").val( localStorage.S15tasks );
+        }        
+        
         $("#task").focus();
     };
-	
+    
     $("#add_task").click(function() {   
         var textbox = $("#task");
         var task = textbox.val();
@@ -14,8 +23,13 @@ $(document).ready(function(){
             textbox.focus();
         } else {  
             // add task to web storage 
-            var tasks = localStorage.E15tasks || "";  // default value of empty string
-            localStorage.E15tasks = tasks.concat( task, "\n" );      
+            var tasks = localStorage.S15tasks || "";  // default value of empty string
+            localStorage.S15tasks = tasks.concat( task, "\n" ); 
+            
+            // store a 21 day expiration
+            var due = new Date();
+            due.setDate( due.getDate() + 21 );
+            localStorage.expiration = due.toDateString();
 
             // clear task text box and re-display tasks
             textbox.val( "" );
@@ -24,7 +38,8 @@ $(document).ready(function(){
     });
     
     $("#clear_tasks").click(function() {
-        localStorage.removeItem( "E15tasks" );
+        localStorage.removeItem( "S15tasks" );
+        localStorage.removeItem( "expiration" );
         $("#task_list").val( "" );
         $("#task").focus();
     }); 
